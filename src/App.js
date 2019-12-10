058ad681411c350e14d1a12d1afc9da455fd721e
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-10 16:39:21 -0600

    Added in all buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
+import Numbers from "../src/components/ButtonComponents/NumberButtons/Numbers"
+import Specials from "../src/components/ButtonComponents/SpecialButtons/Specials"
+import Operators from "../src/components/ButtonComponents/OperatorButtons/Operators"
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
-
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
@@ -18,6 +20,9 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Numbers />
+        <Specials />
+        <Operators />
       </div>
     </div>
   );
